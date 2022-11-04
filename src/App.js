31270908bd6d3d8af45b87e483f1af75d6f7473d
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-11-04 10:30:00 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Testimonials from "./components/Testimonials";
 import TryNoltBoard from "./components/TryNoltBoard";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Testimonials />
       <TryNoltBoard />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <div className="Deer-image"></div>
+      <div className="Footer-options">
+        <div className="Options">
+          <a href="/">Features</a>
+          <a href="/">Pricing</a>
+          <a href="/">Help</a>
+          <a href="/">Terms</a>
+          <a href="/">Privacy</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
